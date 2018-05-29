58c754613f9fcd24dcb9970ec5b9e1e38af2921e
XIEMI2 <mike.xie@oocl.com>
2018-05-29 14:29:58 +0800

    change listener port and create FibController.java

--- src/main/java/com/example/demo/controller/FibController.java
+++ src/main/java/com/example/demo/controller/FibController.java
@@ -1,9 +1,16 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.Fib;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/version")
+@RequestMapping("/fib")
 public class FibController {
+
+    @RequestMapping("/{index}")
+    public int updateMajor(@PathVariable("index") int index) {
+        return new Fib().queryValueByIndex(index);
+    }
 }
